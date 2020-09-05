ff0693c4198c097e2fb8fbba38da55664d413c61
Aomée <juliet.dr@tutanota.com>
2020-09-05 14:08:30 +0200

    navbar modal sign up added

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,8 @@ class ProjectsController < ApplicationController
   def index
     
   end
+
+  def show
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'projects#index'
+  resources :projects
 end
