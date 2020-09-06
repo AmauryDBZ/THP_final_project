e9b75fe5eebcaa401cec47e3c9b93cc86d8d8b91
Lamalle <julienlamalle63100@gmail.com>
2020-09-06 17:40:21 +0200

    Add amazon s3 gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_06_123909) do
-
+ActiveRecord::Schema.define(version: 2020_09_06_135727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
