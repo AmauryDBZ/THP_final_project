a508561f14643a8d280a3c56cccdb129d6aea0d0
Benoit Ferretti <benoitferretti@gmail.com>
2020-09-06 10:34:42 +0200

    creation of project is done without validation of admin

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,7 +9,26 @@ class ProjectsController < ApplicationController
   end
 
   def new
-    
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(
+      user_id: current_user.id,
+      name: params[:name],
+      pitch: params[:pitch], 
+      functionalities: params[:functionalities],
+      value_of_project: params[:value_of_project],
+      number_of_developers_on_project: params[:number_of_developers_on_project],
+      daily_time_spent_on_project_per_developer: params[:daily_time_spent_on_project_per_developer])
+      if @project.save
+        flash[:success] = "Merci ! Nous allons vérifier les informations de votre projet"
+        redirect_to :controller => 'projects', :action => 'index'
+      else
+        flash[:danger] = "Erreur(s) à rectifier pour valider votre projet : #{@project.errors.full_messages.each {|message| message}.join('')}"
+        @project.errors.full_messages.each {|message| puts message}
+        render :action => 'new'
+      end
   end
 
   private 
