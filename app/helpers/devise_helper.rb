69344fe346f5581a48ebfb2fda60565a9971270e
Lamalle <julienlamalle63100@gmail.com>
2020-09-06 11:57:39 +0200

    Navbar done and flash alerts fixed

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -4,11 +4,9 @@ module DeviseHelper
 
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
     html = <<-HTML
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-      #{messages} 
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
+    <div class="alert alert-error alert-block"> <button type="button"
+    class="close" data-dismiss="alert">x</button>
+      #{messages}
     </div>
     HTML
 
