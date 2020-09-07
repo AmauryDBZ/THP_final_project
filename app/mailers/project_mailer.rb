2c201f62785cf156b239b238d12b8e897e64389a
AmauryDBZ <amaury.desbazeille@gmail.com>
2020-09-07 12:08:35 +0200

    update url in mails to redirect to the project view

--- app/mailers/project_mailer.rb
+++ app/mailers/project_mailer.rb
@@ -1,8 +1,8 @@
 class ProjectMailer < ApplicationMailer
   def admin_new_project(project)
     @admins = User.where(is_admin: true)
-    @url = 'https://go-os-develop.herokuapp.com/'
     @project = project
+    @url = 'https://go-os-develop.herokuapp.com/fr/projets/#{@project.slug}'
     @project_holder = @project.user
     emails = @admins.collect(&:email).join(",")
     mail(to: emails, subject: 'Nouveau projet en attente de validation')
