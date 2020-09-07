87ed9375893ae4175029ad242e679cd48429d505
AmauryDBZ <amaury.desbazeille@gmail.com>
2020-09-07 12:14:43 +0200

    quote added

--- app/mailers/project_mailer.rb
+++ app/mailers/project_mailer.rb
@@ -2,7 +2,7 @@ class ProjectMailer < ApplicationMailer
   def admin_new_project(project)
     @admins = User.where(is_admin: true)
     @project = project
-    @url = 'https://go-os-develop.herokuapp.com/fr/projets/#{@project.slug}'
+    @url = "https://go-open-sour-mailer-dyrj1kil2d.herokuapp.com/fr/projets/#{@project.slug}"
     @project_holder = @project.user
     emails = @admins.collect(&:email).join(",")
     mail(to: emails, subject: 'Nouveau projet en attente de validation')
