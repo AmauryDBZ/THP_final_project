643cca7b28a068b6c9768a6fd594dbfdd7865d92
Benoit Ferretti <benoitferretti@gmail.com>
2020-09-07 09:43:57 +0200

    stripe api works

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,5 +3,19 @@ class ChargesController < ApplicationController
   end
 
   def create
+    @amount = 500
+    customer = Stripe::Customer.create({
+      email: params[:stripeEmail],
+      source: params[:stripeToken],
+    })
+  
+    charge = Stripe::Charge.create({
+      customer: customer.id,
+      amount: @amount,
+      description: 'Rails Stripe customer',
+      currency: 'eur',
+    })
+    flash[:success] = "Merci pour votre don !"
+    redirect_to projects_path
   end
 end
