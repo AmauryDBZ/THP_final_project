05f6021216e2cf8a9f39be58356a1cf6f619a1a7
Benoit Ferretti <benoitferretti@gmail.com>
2020-09-07 10:12:22 +0200

    charge update correctly money_earned on project table

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,8 +1,10 @@
 class ChargesController < ApplicationController
   def new
+    @project = Project.friendly.find_by_slug(params[:slug])
   end
 
   def create
+    @project = Project.friendly.find_by_slug(params[:project_id])
     @amount = 500
     customer = Stripe::Customer.create({
       email: params[:stripeEmail],
@@ -15,7 +17,14 @@ class ChargesController < ApplicationController
       description: 'Rails Stripe customer',
       currency: 'eur',
     })
+    @project.update(money_earned: @project.money_earned += (@amount/100))
     flash[:success] = "Merci pour votre don !"
-    redirect_to projects_path
+    redirect_to root_path
+    
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_charge_path
   end
+    
 end
