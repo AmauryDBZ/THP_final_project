be8d1e8231de90a6c2e80d8861ded8e326058a24
Aomée <juliet.dr@tutanota.com>
2020-09-07 18:53:51 +0200

    do not permit users and visitors to see the show page of an non-validated project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,6 +9,15 @@ class ProjectsController < ApplicationController
     @project.update(clicks: @project.clicks += 1)
     @project_holder = User.find(@project.user_id)
     @donations = Donation.where(project_id: @project.id)
+    if current_user 
+      if !@project.validated && current_user != @project.user && !current_user.is_admin 
+        flash[:danger] = "Vous ne pouvez pas accéder à cette page"
+        redirect_to root_path
+      end
+    elsif !@project.validated
+      flash[:danger] = "Vous ne pouvez pas accéder à cette page"
+      redirect_to root_path
+    end
   end
 
   def new
