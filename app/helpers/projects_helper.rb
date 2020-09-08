b07fa039d856faf07c6679cd2d66ca4b5ded7c78
Aomée <juliet.dr@tutanota.com>
2020-09-08 12:07:34 +0200

    project helper factorized

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -3,12 +3,10 @@ module ProjectsHelper
     @project = Project.friendly.find_by_slug(params[:slug])
     if current_user 
       if !@project.validated && current_user != @project.user && !current_user.is_admin 
-        flash[:danger] = "Vous ne pouvez pas accéder à cette page"
-        redirect_to root_path
+        redirection()
       end
     elsif !@project.validated
-      flash[:danger] = "Vous ne pouvez pas accéder à cette page"
-      redirect_to root_path
+      redirection()
     end
   end
 
@@ -16,13 +14,15 @@ module ProjectsHelper
     @project = Project.friendly.find_by_slug(params[:slug])
     if current_user
       if current_user != @project.user && !current_user.is_admin
-        flash[:danger] = "Vous ne pouvez pas accéder à cette page"
-        redirect_to root_path
+        redirection()
       end
     else
-      flash[:danger] = "Vous ne pouvez pas accéder à cette page"
-      redirect_to root_path
+      redirection()
     end  
   end
 
+  def redirection
+    flash[:danger] = "Vous ne pouvez pas accéder à cette page"
+    redirect_to root_path
+  end
 end
