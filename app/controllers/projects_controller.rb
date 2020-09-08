751ca4b7b090dc32ed9d73f368d7c4ac79c988c7
Lamalle <julienlamalle63100@gmail.com>
2020-09-08 09:28:39 +0200

    New project form done with active storage

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -52,6 +52,6 @@ class ProjectsController < ApplicationController
   end
 
   def project_params
-    params.require(:project).permit(:name, :daily_time_spent_on_project_per_developer, :pitch, :functionalities, :value_of_project, :number_of_developers_on_project)
+    params.require(:project).permit(:name, :daily_time_spent_on_project_per_developer, :pitch, :functionalities, :value_of_project, :number_of_developers_on_project, :cover)
   end
 end
