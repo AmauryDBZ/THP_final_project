ce8664909d0760a685ea09d136f01731b5fd1251
Benoit Ferretti <benoitferretti@gmail.com>
2020-09-08 22:06:11 +0200

    transfers features build

---
+++ app/controllers/bank_details_controller.rb
@@ -0,0 +1,26 @@
+class BankDetailsController < ApplicationController
+
+  def new
+    def new
+      @bankdetail = BankDetail.new
+    end
+  end
+
+  def create
+    @bankdetail = BankDetail.new(bank_details_params)
+    @bankdetail.user_id = current_user.id
+      if @bankdetail.save
+        flash[:success] = "Merci d'avoir renseigné vos informations bancaires."
+        redirect_to :controller => 'projects', :action => 'index'
+      else
+        flash[:danger] = "Erreur(s) à rectifier pour valider vos informations bancaires : #{@bankdetail.errors.full_messages.each {|message| message}.join('')}"
+        render :action => 'new'
+      end
+  end
+
+  private
+
+  def bank_details_params
+    params.require(:bank_detail).permit(:bank_name, :iban, :pitch, :branch_code, :bank_code, :account_number, :key)
+  end
+end
