d90d52760b5542d6b812de8b9974aa7092a7b909
AmauryDBZ <amaury.desbazeille@gmail.com>
2020-09-09 10:00:40 +0200

    mailer review

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,4 @@ class User < ApplicationRecord
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
   end
-
-  
-  
 end
--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['MAILJET_API_KEY'],
   :password => ENV['MAILJET_SECRET_KEY'],
-  :domain => 'https://go-os-develop.herokuapp.com',
+  :domain => 'https://go-open-source.herokuapp.com/',
   :address => 'in-v3.mailjet.com',
   :port => 587,
   :authentication => :plain,
