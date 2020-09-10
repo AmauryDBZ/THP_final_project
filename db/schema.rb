f84390d4dcfd6899762fbef09d525701f5b7f0b6
Lamalle <julienlamalle63100@gmail.com>
2020-09-10 12:06:54 +0200

    Added columns url and description in categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_165154) do
+ActiveRecord::Schema.define(version: 2020_09_10_100003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 2020_09_08_165154) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.text "descritpion"
+    t.string "url"
     t.index ["slug"], name: "index_categories_on_slug", unique: true
   end
 
