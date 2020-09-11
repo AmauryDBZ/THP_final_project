cbe54903d4debca0938dcfb6b729bcab4f2a916c
Lamalle <julienlamalle63100@gmail.com>
2020-09-11 09:43:45 +0200

    fixed link to show user in project show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @projects = Project.where(user: @user)
-    @current_user_has_bankdetail = BankDetail.find_by(user_id: current_user.id)
+    @current_user_has_bankdetail = BankDetail.find_by(user_id: @user.id)
     if BankDetail.where(user_id: params[:id]).last
       @bankdetail = BankDetail.where(user_id: params[:id]).last.id
     end
