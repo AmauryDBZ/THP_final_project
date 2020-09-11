ee4361d7235957186d0b8cd3f01b6de72c6b8831
Benoit Ferretti <benoitferretti@gmail.com>
2020-09-11 08:05:20 +0200

    fix current_user has bank details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @projects = Project.where(user: @user)
-    @current_user_has_bankdetail = BankDetail.find_by(user_id: current_user.id)
+    if BankDetail.find_by(user_id: current_user.id)
+      @current_user_has_bankdetail = BankDetail.find_by(user_id: current_user.id)
+    end
     if BankDetail.where(user_id: params[:id]).last
       @bankdetail = BankDetail.where(user_id: params[:id]).last.id
     end
