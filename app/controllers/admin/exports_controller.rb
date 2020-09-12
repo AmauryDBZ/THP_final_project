ef51eea9a32e7bddb0cf6c78a67a28c40571e3a4
AmauryDBZ <amaury.desbazeille@gmail.com>
2020-09-12 12:53:54 +0200

    features export to csv done

--- app/controllers/admin/exports_controller.rb
+++ app/controllers/admin/exports_controller.rb
@@ -1,13 +1,23 @@
 class Admin::ExportsController < ApplicationController
-  include Admin::ExportsHelper
 
   def new
-    csv_user('users_GOS')
+    @users = User.all
+    @donations = Donation.all
+    table = params[:table]
     respond_to do |format|
       format.html {
         redirect_to admin_users_fr_path
       }
-      format.js { }
+      format.csv {
+        puts 'export controller csv'
+        if table == "donation"
+          send_data @donations.to_csv(table), filename: "#{table}-#{Date.today}.csv"
+        elsif table == "user"
+          send_data @users.to_csv(table), filename: "#{table}-#{Date.today}.csv"
+        else
+          flash[:warning] = "an error occur during the export process"
+        end
+      }
     end
   end
 
--- app/helpers/admin/exports_helper.rb
+++ app/helpers/admin/exports_helper.rb
@@ -1,15 +1,3 @@
 module Admin::ExportsHelper
-  require 'csv'
-
-  def csv_user(file_name)
-    user_table = []
-    user_table = User.all
-    CSV.open("#{file_name}.csv", "w") do |f|
-      f << user_table.first.attributes.map { |a, v| a }
-      user_table.each do |user|
-        f << user.attributes.map { |a, v| v }
-      end
-    end
-  end
 
 end
