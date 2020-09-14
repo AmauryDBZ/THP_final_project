4f682a34fd8ea90fc72e7795aceb4096873925b0
AmauryDBZ <amaury.desbazeille@gmail.com>
2020-09-14 21:22:23 +0200

    only last month donations appears

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,8 +1,8 @@
 module ProjectsHelper
   def cannot_see_before_validation
     @project = Project.friendly.find_by_slug(params[:slug])
-    if current_user 
-      if !@project.validated && current_user != @project.user && !current_user.is_admin 
+    if current_user
+      if !@project.validated && current_user != @project.user && !current_user.is_admin
         redirection()
       end
     elsif !@project.validated
@@ -18,11 +18,27 @@ module ProjectsHelper
       end
     else
       redirection()
-    end  
+    end
   end
 
   def redirection
     flash[:danger] = "Vous ne pouvez pas accéder à cette page"
     redirect_to root_path
+  end
+
+  def monthly_donation
+    @last_month_donation = []
+    puts '------------------ project helper ------------------------'
+    puts Donation.where(project_id: @project.id)
+    Donation.where(project_id: @project.id).each do |d|
+      puts "created at #{d.created_at}"
+      puts Time.now
+      if Time.now - d.created_at < (2*24*3600)
+        @last_month_donation << d
+      end
+      puts '--------------------------'
+      puts @last_month_donation
+    end
+    return @last_month_donation
   end
 end
